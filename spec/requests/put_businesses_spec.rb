3fed931cd002fed003cfed6953df6091ee6b5f02
Ryan Duff <ryanduff613@gmail.com>
2020-06-12 13:56:03 -0700

    add test for put route to verify message is delivered on success, all tests passing

--- spec/requests/put_businesses_spec.rb
+++ spec/requests/put_businesses_spec.rb
@@ -8,6 +8,7 @@ describe "post a business route", :type => :request do
   end
 
   it "returns a success message business record is updated" do
+    expect(JSON.parse(response.body)).to(include ("message"))
   end
 
   it "changes an attribute on a business record" do
@@ -18,4 +19,26 @@ describe "post a business route", :type => :request do
   it 'returns status 200 ok when update is succesfull' do
     expect(response).to have_http_status(:ok)
   end
+end
+
+
+
+describe "post a business route errors", :type => :request do
+  before do
+    post '/businesses', params: {:name => 'Hotlips Pizza', :business_type => 'Restaurant', :phone => '503 234 9999', :address => '2211 SE Hawthorne Blvd, Portland OR, 97214', :website => 'https://www.hotlipspizza.com/'}
+    @business_id = JSON.parse(response.body)['id']
+    put "/businesses/#{@business_id}", params: {:name => nil}
+  end
+
+  # it "returns a success message business record is updated" do
+  # end
+
+  # it "changes an attribute on a business record" do
+  #   get "/businesses/#{@business_id}"
+  #   expect(JSON.parse(response.body)['name']).to(eq('Dominos Pizza'))
+  # end
+
+  # it 'returns status 200 ok when update is succesfull' do
+  #   expect(response).to have_http_status(:ok)
+  # end
 end
