209b8636fabbea516d831b86a77fe2a6db5d0498
Ryan Duff <ryanduff613@gmail.com>
2020-06-12 12:47:42 -0700

    create tests for get business by id route

--- spec/requests/get_businessses_spec.rb
+++ spec/requests/get_businessses_spec.rb
@@ -3,11 +3,6 @@ require 'rails_helper'
 describe "get all businesses route", :type => :request do
   let!(:businesses) {FactoryBot.create_list(:business, 10)}
   before {get '/businesses'}
-  # before do get '/businesses'
-  #   post '/businesses', params: {:name => "hotlips", :business_type => 'pizza', :phone => '555.555.5555', :address => '2121 SE Hawthorne blvd', :website => "http://www.hotlipspizza.com"}
-  #   post '/businesses', params: {:name => "hotlips", :business_type => 'pizza', :phone => '555.555.5555', :address => '2121 SE Hawthorne blvd', :website => "http://www.hotlipspizza.com"}
-  #   get '/businesses'
-  # end
 
   it 'returns all businesses' do
     expect(JSON.parse(response.body).size).to(eq(10))
@@ -18,8 +13,24 @@ describe "get all businesses route", :type => :request do
   end
 end
 
+describe "get businesses by id route", :type => :request do
+  before do
+    post '/businesses', params: {:name => "Hotlips Pizza", :business_type => 'pizza', :phone => '555.555.5555', :address => '2121 SE Hawthorne blvd', :website => "http://www.hotlipspizza.com"}
+    get '/businesses'
+  end
+
+  it "returns a single business object" do
+    business=Business.last
+    expect(business.name).to(eq('Hotlips Pizza'))
+  end
+
+  it "returns 200 ok status" do
+    business = Business.last
+    expect(response).to have_http_status(:ok)
+  end
+end
+
 describe "error handling on get all businesses route", :type => :request do
-  
   before {get '/businesses/10'}
 
   it 'returns an error message if user makes get request for a business that has no record in database ' do
