796a27d0db2eb55614df114857f100a81349757d
Ryan Duff <ryanduff613@gmail.com>
2020-06-12 13:16:26 -0700

    create tests for post route error, all test passing

--- spec/requests/post_businesses_spec.rb
+++ spec/requests/post_businesses_spec.rb
@@ -13,3 +13,17 @@ describe "post a business route", :type => :request do
     expect(response).to have_http_status(:created)
   end
 end
+
+describe "post a business route error", :type => :request do
+  before do
+    post '/businesses', params: {:name => nil, :business_type => 'Restaurant', :phone => '503 234 9999', :address => '2211 SE Hawthorne Blvd, Portland OR, 97214', :website => 'https://www.hotlipspizza.com/'}
+  end
+
+  it "returns an error message" do
+    expect(JSON.parse(response.body)).to(eq({"message"=>"Validation failed: Name can't be blank"}))
+  end
+
+  it "returns a created status" do
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+end
