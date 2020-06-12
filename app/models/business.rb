4665e0309219182e325599702a419161af3c4721
Ryan Duff <ryanduff613@gmail.com>
2020-06-12 16:23:47 -0700

    add scope for name and add search function

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,9 @@
 class Business < ApplicationRecord
 validates_presence_of :name, :business_type, :phone, :address, :website
+scope :search_name, -> (name) {where(name: name)}
+
+  def self.search(search_term)
+    search_name(search_term)
+  end
 
 end
