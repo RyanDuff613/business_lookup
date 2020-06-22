6738bf5a24402c0472f4de546e03398ba681e760
Ryan Duff <ryanduff613@gmail.com>
2020-06-21 23:02:56 -0700

    add custom route to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   match '*path', :to => 'application#routing_error', via: [:post]
   get "/search" => 'businesses#search'
+  get "/" => 'businesses#index'
   
   resources :businesses do
   end
