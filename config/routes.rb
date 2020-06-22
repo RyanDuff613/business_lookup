63726fd8c7bd3dc71a7883ac551b321e879fbad3
Ryan Duff <ryanduff613@gmail.com>
2020-06-21 22:58:08 -0700

    move custom routes above 'resources :businesses' in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :businesses do
-  end
   match '*path', :to => 'application#routing_error', via: [:post]
   get "/search" => 'businesses#search'
+  
+  resources :businesses do
+  end
   
 end
